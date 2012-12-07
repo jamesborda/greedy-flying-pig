e42ebb419ea88a3eee882039fdec9d41186bf43a
James Borda <james@mopedog.com>
2012-12-07 15:39:06 -0500

    Lots of changes from HW 5 template

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ connect('mydata', host=os.environ.get('MONGOLAB_URI'))
 print "Connecting to MongoLabs"
 
 
-categories = ['web','physical computing','software','video','music','installation','assistive technology','developing nations','business','social networks']
+# categories = ['web','physical computing','software','video','music','installation','assistive technology','developing nations','business','social networks']
 
 # --------- Routes ----------
 
@@ -28,116 +28,50 @@ categories = ['web','physical computing','software','video','music','installatio
 @app.route("/")
 def index():
 
-	log = models.Log() #JB these three lines added
-	log.text = 'yo yo'
-	log.save()
-
-	#get all logs
-	myLogs = models.Log.objects() #everything inside 
-
-
-	# render the template, pass in the animals dictionary refer to it as 'animals'
 	templateData = {
-		'ideas' : models.Idea.objects(),
-		'categories' : categories,
-		'logs' : myLogs
+		'projects' : models.Project.objects(),
 	}
 	return render_template("main.html", **templateData)
 
-@app.route("/category/<cat_name>")
-def by_category(cat_name):
-
-	try:
-		ideas = models.Idea.objects(categories=cat_name)
-	except:
-		abort(404)
-
-	templateData = {
-		'current_category' : {
-			'slug' : cat_name,
-			'name' : cat_name.replace('_',' ')
-		},
-		'ideas' : ideas,
-		'categories' : categories
-	}
-
-	return render_template('category_listing.html', **templateData)
-
-@app.route("/ideas/add", methods=["POST"])
-def newidea():
+@app.route("/projects/add", methods=["POST"])
+def newproject():
 	
-	app.logger.debug('idea form response data')
+	app.logger.debug('project form response data')
 	app.logger.debug(request.form)
-	app.logger.debug('list of submitted categories')
-	app.logger.debug(request.form.getlist('categories'))
+	# app.logger.debug('list of submitted categories')
+	# app.logger.debug(request.form.getlist('categories'))
 
 	# get form data - create new idea
-	idea = models.Idea()  #JB - inside of the models.py object, we're going to get the class Idea
-	idea.creator = request.form.get('creator','anonymous')
-	idea.title = request.form.get('title','no title')
-	idea.slug = slugify(idea.title + " " + idea.creator)
-	idea.idea = request.form.get('idea','')
-	idea.categories = request.form.getlist('categories')
+	project = models.Project()  #JB - inside of the models.py object, we're going to get the class Idea
+	project.user = request.form.get('user','Anonymous')
+	project.title = request.form.get('title','No Title')
+	project.slug = slugify(project.title + " " + project.user)
+	# idea.idea = request.form.get('idea','')
+	# idea.categories = request.form.getlist('categories')
 	
-	idea.save()
+	project.save()
 
-	return redirect('/ideas/%s' % idea.slug)
+	return redirect('/projects/%s' % project.slug)
 
-@app.route("/ideas/<idea_slug>") #JB - this slug can be named anything...it's just for our use
-def idea_display(idea_slug):
+@app.route("/projects/<project_slug>") #JB - this slug can be named anything...it's just for our use
+def project_display(project_slug):
 
 	## JB - You could, e.g., @app.route("/ideas/<idea_slug>/<color>/<shape>/")
 	## def idea_display(idea_slug, color, shape):
 
 	# get idea by idea_slug
 	try:
-		idea = models.Idea.objects.get(slug=idea_slug)
+		project = models.Project.objects.get(slug=project_slug)
 	except:
 		abort(404)
 
 	# prepare template data
 	templateData = {
-		'idea' : idea
+		'project' : project
 	}
 
 	# render and return the template
-	return render_template('idea_entry.html', **templateData)
-	
-@app.route("/ideas/<idea_id>/comment", methods=['POST'])
-def idea_comment(idea_id):
-
-	name = request.form.get('name')
-	comment = request.form.get('comment')
-
-	if name == '' or comment == '':
-		# no name or comment, return to page (JB - this page)
-		return redirect(request.referrer)
-
-
-	#get the idea by id
-	try:
-		idea = models.Idea.objects.get(id=idea_id)
-	except:
-		# error, return to where you came from
-		return redirect(request.referrer)
-
-
-	# create comment
-	comment = models.Comment()
-	comment.name = request.form.get('name')
-	comment.comment = request.form.get('comment')
-	
-	# append comment to idea
-	idea.comments.append(comment)
-	idea.save()
-
-	return redirect('/ideas/%s' % idea.slug)
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html'), 404
-
+	return render_template('project.html', **templateData)
 
 # slugify the title 
 # via http://flask.pocoo.org/snippets/5/
@@ -148,6 +82,117 @@ def slugify(text, delim=u'-'):
 	for word in _punct_re.split(text.lower()):
 		result.extend(unidecode(word).split())
 	return unicode(delim.join(result))
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+## -------------- PREVIOUS ROUTES --------------------
+
+# @app.route("/category/<cat_name>")
+# def by_category(cat_name):
+
+# 	try:
+# 		ideas = models.Idea.objects(categories=cat_name)
+# 	except:
+# 		abort(404)
+
+# 	templateData = {
+# 		'current_category' : {
+# 			'slug' : cat_name,
+# 			'name' : cat_name.replace('_',' ')
+# 		},
+# 		'ideas' : ideas,
+# 		'categories' : categories
+# 	}
+
+# 	return render_template('category_listing.html', **templateData)
+
+# @app.route("/ideas/add", methods=["POST"])
+# def newidea():
+	
+# 	app.logger.debug('idea form response data')
+# 	app.logger.debug(request.form)
+# 	app.logger.debug('list of submitted categories')
+# 	app.logger.debug(request.form.getlist('categories'))
+
+# 	# get form data - create new idea
+# 	idea = models.Idea()  #JB - inside of the models.py object, we're going to get the class Idea
+# 	idea.creator = request.form.get('creator','anonymous')
+# 	idea.title = request.form.get('title','no title')
+# 	idea.slug = slugify(idea.title + " " + idea.creator)
+# 	idea.idea = request.form.get('idea','')
+# 	idea.categories = request.form.getlist('categories')
+	
+# 	idea.save()
+
+# 	return redirect('/ideas/%s' % idea.slug)
+
+# @app.route("/ideas/<idea_slug>") #JB - this slug can be named anything...it's just for our use
+# def idea_display(idea_slug):
+
+# 	## JB - You could, e.g., @app.route("/ideas/<idea_slug>/<color>/<shape>/")
+# 	## def idea_display(idea_slug, color, shape):
+
+# 	# get idea by idea_slug
+# 	try:
+# 		idea = models.Idea.objects.get(slug=idea_slug)
+# 	except:
+# 		abort(404)
+
+# 	# prepare template data
+# 	templateData = {
+# 		'idea' : idea
+# 	}
+
+# 	# render and return the template
+# 	return render_template('idea_entry.html', **templateData)
+	
+# @app.route("/ideas/<idea_id>/comment", methods=['POST'])
+# def idea_comment(idea_id):
+
+# 	name = request.form.get('name')
+# 	comment = request.form.get('comment')
+
+# 	if name == '' or comment == '':
+# 		# no name or comment, return to page (JB - this page)
+# 		return redirect(request.referrer)
+
+
+# 	#get the idea by id
+# 	try:
+# 		idea = models.Idea.objects.get(id=idea_id)
+# 	except:
+# 		# error, return to where you came from
+# 		return redirect(request.referrer)
+
+
+# 	# create comment
+# 	comment = models.Comment()
+# 	comment.name = request.form.get('name')
+# 	comment.comment = request.form.get('comment')
+	
+# 	# append comment to idea
+# 	idea.comments.append(comment)
+# 	idea.save()
+
+# 	return redirect('/ideas/%s' % idea.slug)
+
+
+# @app.errorhandler(404)
+# def page_not_found(error):
+#     return render_template('404.html'), 404
+
+
+# # slugify the title 
+# # via http://flask.pocoo.org/snippets/5/
+# _punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
+# def slugify(text, delim=u'-'):
+# 	"""Generates an ASCII-only slug."""
+# 	result = []
+# 	for word in _punct_re.split(text.lower()):
+# 		result.extend(unidecode(word).split())
+# 	return unicode(delim.join(result))
 
 
 # --------- Server On ----------
--- models.py
+++ models.py
@@ -2,29 +2,55 @@
 from mongoengine import *
 from datetime import datetime
 
-class Log(Document):
-	text = StringField()
-	timestamp = DateTimeField(default=datetime.now()) 
+# class Log(Document):
+# 	text = StringField()
+# 	timestamp = DateTimeField(default=datetime.now()) 
 
-class Comment(EmbeddedDocument): #something you put inside another doc (you never use .save on this)
-	name = StringField()
-	comment = StringField()
-	timestamp = DateTimeField(default=datetime.now())
+# class Comment(EmbeddedDocument): #something you put inside another doc (you never use .save on this)
+# 	name = StringField()
+# 	comment = StringField()
+# 	timestamp = DateTimeField(default=datetime.now())
 
-class Idea(Document):
+# class Idea(Document):
 
-	creator = StringField(max_length=120, required=True)
-	title = StringField(max_length=120, required=True)
-	slug = StringField()
-	idea = StringField(required=True)
+# 	creator = StringField(max_length=120, required=True)
+# 	title = StringField(max_length=120, required=True)
+# 	slug = StringField()
+# 	idea = StringField(required=True)
 
-	# Category is a list of Strings
-	categories = ListField(StringField(max_length=30))
+# 	# Category is a list of Strings
+# 	categories = ListField(StringField(max_length=30))
 
-	# Comments is a list of Document type 'Comments' defined above
-	comments = ListField( EmbeddedDocumentField(Comment) )
+# 	# Comments is a list of Document type 'Comments' defined above
+# 	comments = ListField( EmbeddedDocumentField(Comment) )
 
-	# Timestamp will record the date and time idea was created.
-	timestamp = DateTimeField(default=datetime.now())
+# 	# Timestamp will record the date and time idea was created.
+# 	timestamp = DateTimeField(default=datetime.now())
 	
+class User(Document):
+	name = StringField()
+	total_paid = DecimalField()
+	# email = EmailField()
+	# password = StringField()
 
+
+class Purchase(EmbeddedDocument):
+	user = ListField (EmbeddedDocumentField(User))
+	dollar_amount = DecimalField()
+	description = StringField()
+	date = DateTimeField(default=datetime.now())
+	quantity = IntField()
+	# url = URLField()
+
+
+class Project(Document):
+	title = StringField()
+	purchase = ListField (EmbeddedDocumentField(Purchase)) #use .append 
+	user = ListField (EmbeddedDocumentField(User)) #ReferenceField
+	slug = StringField()
+	total_cost = DecimalField()
+	cost_per_person = DecimalField()
+	# start_date = DateTimeField(default=datetime.now())
+	# end_date = DateTimeField()
+	# course = StringField()  # the ITP class the project was assigned for
+	# password = StringField(default=Project.name)
